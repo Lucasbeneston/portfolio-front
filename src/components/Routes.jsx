93541a67c2b37e6a4acb51b7e1bc7f8a301f2658
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-16 21:29:26 +0100

    :sparkles: build project :sparkles:

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ProjetsList from "./pages/Projects/Projects";
 import Projet from "./pages/Projet/Projet";
@@ -8,14 +8,12 @@ import NoMatch from "./pages/NoMatch/NoMatch";
 
 export default function Routes() {
   return (
-    <HashRouter basename="/">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/projets" component={ProjetsList} />
-        <Route exact path="/projets/:urlName" component={Projet} />
-        <Route exact path="/informations" component={Informations} />
-        <Route path="*" component={NoMatch} />
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/projets" component={ProjetsList} />
+      <Route exact path="/projets/:urlName" component={Projet} />
+      <Route exact path="/informations" component={Informations} />
+      <Route path="*" component={NoMatch} />
+    </Switch>
   );
 }
