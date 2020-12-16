ef07822731b493f2f350eddc739d87c27733ad79
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-16 20:04:45 +0100

    :recycle: add 404.html :recycle:

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, HashRouter, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ProjetsList from "./pages/Projects/Projects";
 import Projet from "./pages/Projet/Projet";
@@ -9,13 +9,11 @@ import NoMatch from "./pages/NoMatch/NoMatch";
 export default function Routes() {
   return (
     <Switch>
-      <HashRouter basename="/portfolio-front">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/projets" component={ProjetsList} />
-        <Route exact path="/projets/:urlName" component={Projet} />
-        <Route exact path="/informations" component={Informations} />
-        <Route path="*" component={NoMatch} />
-      </HashRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/projets" component={ProjetsList} />
+      <Route exact path="/projets/:urlName" component={Projet} />
+      <Route exact path="/informations" component={Informations} />
+      <Route path="*" component={NoMatch} />
     </Switch>
   );
 }
