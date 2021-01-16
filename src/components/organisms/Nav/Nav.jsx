65f06f7bd447cff098eac1d029b5083384d35b8a
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2021-01-16 15:27:42 +0100

    :sparkles: Add return button :sparkles:

--- src/components/organisms/Nav/Nav.jsx
+++ src/components/organisms/Nav/Nav.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
 import { HashLink } from "react-router-hash-link";
+import { useHistory, useLocation } from "react-router-dom";
 import MenuContext from "../../../contexts/MenuContext";
 import BurgerMenu from "../../molecules/BurgerMenu/BurgerMenu";
 import MenuSlide from "../../molecules/MenuSlide/MenuSlide";
+import ArrowLeft from "../../atoms/SVG/ArrowLeft";
 
 import "./Nav.scss";
 
 export default function Nav() {
   const context = useContext(MenuContext);
+  const history = useHistory();
+  const location = useLocation();
+
   const { open, setOpen } = context;
+  const { pathname } = location;
 
   const switchOpen = () => {
     setOpen(!open);
@@ -23,6 +29,19 @@ export default function Nav() {
 
   return (
     <nav className={`nav ${open ? "open" : ""}`}>
+      {pathname !== "/" ? (
+        <div
+          className={`nav_goBack ${open ? "open" : ""}`}
+          role="button"
+          tabIndex={0}
+          onClick={() => history.goBack()}
+          onKeyPress={() => history.goBack()}
+        >
+          <ArrowLeft />
+          <span>Retour</span>
+        </div>
+      ) : null}
+
       <HashLink onClick={click} to="/#" className="nav_title">
         Lucas BENESTON
       </HashLink>
